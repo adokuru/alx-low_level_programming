815f5d9922b3bba3cb628862f9bef2b37605a5f5
David Adokuru <45779326+adokuru@users.noreply.github.com>
2022-07-06 19:37:57 +0100

    Task 0-3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-void print_remaining_days(int, int, int);
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-int convert_day(int, int);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
